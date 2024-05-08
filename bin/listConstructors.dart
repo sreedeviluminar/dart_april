06505aa0edb99de1683cfa0248511aeb76339ba5
sreedeviluminar <sreedevi@luminartechnolab.com>
2024-05-08 13:06:24 +0530

    commit

--- bin/listConstructors.dart
+++ bin/listConstructors.dart
@@ -28,9 +28,9 @@ void main() {
   list7[9] = 21;
 
   List list8 = List.unmodifiable({1,2,3,4,5,6,7});
- //list8[5] = 12;
- //list8.remove(7);
-  //list8.add(10);
+  //list8[5] = 12;
+  //list8.remove(7);
+  list8.add(10);
 
   print('list1 = $list1');
   print('list2 = $list2');
